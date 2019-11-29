91e8a2eef17ec3a92d398496934fb16c73098fc0
Solal Thoniel <sthoniel@insa-lyon.fr>
2019-11-29 11:58:40 +0100

    Progres du code, il manque a faire remonter les mouvements valides

--- src/choose_move.pl
+++ src/choose_move.pl
@@ -1,24 +1,64 @@
 
 
-choix_mouvement(Board, Player, Mouvement) :-
-	trouver_mouvements(Board, Player, MouvList),
-	evaluer_mouvements(Board, Player, MouvList, Mouvement).
+choix_Mouvement(Board, Player, Mouvement) :-
+	trouver_Mouvements(Board, Player, MouvList),
+	evaluer_Mouvements(Board, Player, MouvList, Mouvement).
 
 
-trouver_mouvements(Board, Player, MouvList) :- .
+trouver_Mouvements(Board, Player, MouvList).
 	
-	
-recup_mouv_possible(Board, [], Player, MouvList).
-recup_mouv_possible(Board, [H|T], Player, MouvList) :- ajout_mouv_possible(Board, [H|T], Player, MouvList), recup_mouv_possible(Board, T, Player, MouvList).
+%Parcours de toutes les cases du plateau et test de l'ajout de la case comme coup possible
+recup_Mouv_possible(Board, [], Player, MouvList).
+recup_Mouv_possible(Board, [H|T], Player, MouvList) :- ajout_Mouv_possible(Board, H, Player, MouvList), recup_Mouv_possible(Board, T, Player, MouvList).
 
 
-ajout_mouv_possible([H|T], Player, MouvList) :- H==vide, test_mouv_possible([H|T], Player, MouvList).
-ajout_mouv_possible([H|T], Player, MouvList) :- H\==vide.
+%Si la case testee est vide alors cela peut etre un coup possible
+ajout_Mouv_possible(Board, Mouv, Player, MouvList) :- nth1(Mouv, Board, MouvCase), nonvar(MouvCase), test_Mouv_possible(Board, Mouv, Player, MouvList).
 
-test_mouv_possible([H|T], Player, MouvList) :- .
-test_mouv_possible([H|T], Player, MouvList) :- .
+%Si la case testee n'est pas vide, alors elle n'est pas exploitable pour un coup
+ajout_Mouv_possible(Board, Mouv, Player, MouvList).
 
 
+%on teste les parcours dans les 8 directions
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, -9).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, -8).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, -7).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, -1).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, 1).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, 7).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, 8).
+test_Mouv_possible(Board, Mouv, Player, MouvList) :- test_premier_voisin(Board, Mouv, Player, MouvList, 9).
 
-evaluer_mouvements(Board, Player, [H|T], H).
+
+%On regarde le premier voisin de la case du mouvement avant de lancer la recursion.
+test_premier_voisin(Board, Mouv, Player, MouvList, Dir) :- nonSortis(Mouv, Dir), nouvPos is Mouv + Dir, 
+nth1(nouvPos, Board, nouvCase), var(nouvCase), nouvCase\=Player, test_voisin_suivant(Board, nouvPos, Player, MouvList, Dir).
+
+
+%Sortie de la recursion positive
+test_voisin_suivant(Board, Mouv, Player, MouvList, Dir) :- nonSortis(Mouv, Dir), nouvPos is Mouv + Dir, 
+nth1(nouvPos, Board, nouvCase), var(nouvCase), nouvCase==Player.
+
+%Sortie de la recursion negative
+test_voisin_suivant(Board, Mouv, Player, MouvList, Dir) :- (\nonSortis(Mouv, Dir)); ( nouvPos is Mouv + Dir, 
+nth1(nouvPos, Board, nouvCase), nonvar(nouvCase) ).
+
+%On continue la recursion
+test_voisin_suivant(Board, Mouv, Player, MouvList, Dir) :- nonSortis(Mouv, Dir), nouvPos is Mouv + Dir, 
+nth1(nouvPos, Board, nouvCase), var(nouvCase), nouvCase\=Player, test_voisin_suivant(Board, nouvPos, Player, MouvList, Dir).
+
+
+%Test si on sort du plateau depuis un case vers une direction choisie
+nonSortis(Mouv, Dir) :- Dir = -9, Mouv\=1, Mouv\=2, Mouv\=3, Mouv\=4, Mouv\=5, Mouv\=6, Mouv\=7, Mouv\=8, Mouv\=9, Mouv\=17, Mouv\=25, Mouv\=33, Mouv\=41, Mouv\=49, Mouv\=57.
+nonSortis(Mouv, Dir) :- Dir = -8, Mouv\=1, Mouv\=2, Mouv\=3, Mouv\=4, Mouv\=5, Mouv\=6, Mouv\=7, Mouv\=8.
+nonSortis(Mouv, Dir) :- Dir = -7, Mouv\=1, Mouv\=2, Mouv\=3, Mouv\=4, Mouv\=5, Mouv\=6, Mouv\=7, Mouv\=8, Mouv\=16, Mouv\=24, Mouv\=32, Mouv\=40, Mouv\=48, Mouv\=56, Mouv\=64.
+nonSortis(Mouv, Dir) :- Dir = -1, Mouv\=1, Mouv\=9, Mouv\=17, Mouv\=25, Mouv\=33, Mouv\=41, Mouv\=49, Mouv\=57.
+nonSortis(Mouv, Dir) :- Dir = 1, Mouv\=8, Mouv\=16, Mouv\=24, Mouv\=32, Mouv\=40, Mouv\=48, Mouv\=56, Mouv\=64.
+nonSortis(Mouv, Dir) :- Dir = 7, Mouv\=1, Mouv\=9, Mouv\=17, Mouv\=25, Mouv\=33, Mouv\=41, Mouv\=49, Mouv\=57, Mouv\=58, Mouv\=59, Mouv\=60, Mouv\=61, Mouv\=62, Mouv\=63, Mouv\=64.
+nonSortis(Mouv, Dir) :- Dir = 8, Mouv\=57, Mouv\=58, Mouv\=59, Mouv\=60, Mouv\=61, Mouv\=62, Mouv\=63, Mouv\=64.
+nonSortis(Mouv, Dir) :- Dir = 9, Mouv\=57, Mouv\=58, Mouv\=59, Mouv\=60, Mouv\=61, Mouv\=62, Mouv\=63, Mouv\=64, Mouv\=8, Mouv\=16, Mouv\=24, Mouv\=32, Mouv\=40, Mouv\=48, Mouv\=56.
+
+
+%Prend le premier mouvement possible
+evaluer_Mouvements(Board, Player, [H|T], H).
 
