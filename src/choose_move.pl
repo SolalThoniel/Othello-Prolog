f7935fa30df0394d3b79a430a2a13803596d8e50
Solal Thoniel <solalthoniel@gmail.com>
2019-12-06 14:28:20 +0100

    Derniere version

--- src/choose_move.pl
+++ src/choose_move.pl
@@ -26,7 +26,7 @@ choix_Mouvement_Directions(Board, Player, PlayerType, MouvList, MouvementDirecti
 directions_Mouvement(Board, Player, Mouvement, MouvementDirections).
 
 
-%Le once(length) force la list à choisir une taille et elimine les elements inconnu qui pourraits etre solution.
+%Le once(length) force la liste à choisir une taille et elimine les elements inconnu qui pourraits etre solution.
 trouver_Mouvements(Board, Player, MouvList) :- once(recup_Mouv_possible(Board,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64], Player, MouvList)), once(length(MouvList, Taille)).
 
 
